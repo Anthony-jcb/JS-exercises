5fc76d3519326720078f14e5a42741c84ff88573
Anthony-jcb <thonyjbc@gmail.com>
2021-07-12 23:16:30 -0400

    Exercise Finishedgit add .

--- ejercicio_reloj_alarma/crono_alarm.js
+++ ejercicio_reloj_alarma/crono_alarm.js
@@ -4,7 +4,10 @@ export function Cronometer($Display, $startButton, $stopButton) {
   let Tempo;
 
   D.addEventListener("click", (event) => {
-    if (event.target.matches($startButton)) {
+    if (
+      event.target.matches($startButton) ||
+      event.target.matches(`${$startButton} *`)
+    ) {
       Tempo = setInterval(() => {
         let clockTime = new Date().toLocaleTimeString();
         D.querySelector($Display).innerHTML = `<h3>${clockTime}</h3>`;
@@ -13,32 +16,45 @@ export function Cronometer($Display, $startButton, $stopButton) {
       event.target.disabled = true;
     }
 
-    if (event.target.matches($stopButton)) {
+    if (
+      event.target.matches($stopButton) ||
+      event.target.matches(`${$stopButton} *`)
+    ) {
       clearInterval(Tempo);
-      D.querySelector($Display).innerHTML = null;
+      D.querySelector($Display).innerHTML = `<h3>00:00:00</h3>`;
       D.querySelector($startButton).disabled = false;
     }
   });
 }
 
-export function Alarm($startButton, $stopButton) {
+export function Alarm($newAlarm, $stopAlarm) {
   let alarm_sound;
 
   D.addEventListener("click", (event) => {
-    if (event.target.matches($startButton)) {
+    if (
+      event.target.matches($newAlarm) ||
+      event.target.matches(`${$newAlarm} *`)
+    ) {
+      try {
+        alarm_sound.pause();
+      } catch (error) {}
+
       alarm_sound = new Audio("./assets/alarm.mp3");
       alarm_sound.play();
       alarm_sound.loop = true;
 
-      event.disabled = true;
+      event.target.disabled = true;
     }
   });
 
   D.addEventListener("click", (event) => {
-    if (event.target.matches($stopButton)) {
+    if (
+      event.target.matches($stopAlarm) ||
+      event.target.matches(`${$stopAlarm} *`)
+    ) {
       alarm_sound.pause();
-      
-      D.querySelector($startButton).disabled = false;
+
+      D.querySelector($newAlarm).disabled = false;
     }
   });
 }
--- ejercicio_reloj_alarma/index_dom.js
+++ ejercicio_reloj_alarma/index_dom.js
@@ -1,5 +1,4 @@
-import { Alarm, Cronometer } from "./crono_alarm.js";
+import { Cronometer, Alarm } from "./crono_alarm.js";
 
 Cronometer("#display_crono", "#start_crono", "#stop_crono");
-
-Alarm("#new_alarm", "#stop_alarm")
+Alarm("#new_alarm", "#stop_alarm");
