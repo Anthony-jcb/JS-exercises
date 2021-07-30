9d436b5187b48215acb06b32d357d192eccaf1cb
Anthony-jcb <thonyjbc@gmail.com>
2021-07-29 22:51:48 -0400

    Exercise finished

--- ejercicio_user_agent_&_internet_events/connection_state.js
+++ ejercicio_user_agent_&_internet_events/connection_state.js
@@ -1,3 +1,35 @@
-export default function connectionState() {
-  
+export default function connectionState(Elements) {
+  const $container = document.querySelector(Elements.container),
+    $message_container = document.querySelector(Elements.messageContainer),
+    $onlineIcon = document.querySelector(Elements.onlineIcon),
+    $offlineIcon = document.querySelector(Elements.offlineIcon);
+
+  function networkStatus() {
+    if (navigator.onLine) {
+      $container.classList.toggle("hide");
+      $message_container.innerHTML = "Conectado a internet";
+
+      $onlineIcon.classList.toggle("none");
+      setTimeout(() => {
+        $container.classList.toggle("hide");
+      }, 3000);
+      setTimeout(() => {
+        $onlineIcon.classList.toggle("none");
+      }, 3500);
+    } else {
+      $container.classList.toggle("hide");
+      $message_container.innerHTML = "No tienes conexión";
+
+      $offlineIcon.classList.toggle("none");
+      setTimeout(() => {
+        $container.classList.toggle("hide");
+      }, 3000);
+      setTimeout(() => {
+        $offlineIcon.classList.toggle("none");
+      }, 3500);
+    }
+  }
+
+  window.addEventListener("online", networkStatus);
+  window.addEventListener("offline", networkStatus);
 }
--- ejercicio_user_agent_&_internet_events/index.js
+++ ejercicio_user_agent_&_internet_events/index.js
@@ -1,8 +1,14 @@
 import userDeviceInfo from "./device_detection.js";
-import connectionState from "./connection_state.js"
+import connectionState from "./connection_state.js";
 
 const D = document;
 
 D.addEventListener("DOMContentLoaded", () => {
   userDeviceInfo("user_device");
-})
+  connectionState({
+    container: "#conn_state",
+    messageContainer: "#message_container",
+    onlineIcon: "#onlineIcon",
+    offlineIcon: "#offlineIcon",
+  });
+});
